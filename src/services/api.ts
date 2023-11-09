6d687e4b174aa424a8a3210b701f2ada5803d80a
Vingren <132018575+VingrenSousa@users.noreply.github.com>
2023-11-08 21:09:05 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axion from 'axios'
 export const api = axion.create({
     baseURL: 'https://api.clarifai.com',
     headers:{
-        "Authorization":"key "+process.env.EXPO_PUBLIC_API_KEY
+        "Authorization":"key "+process.env.EXPO_PUBLIC_API_KEY_ID
     }
 
 })
